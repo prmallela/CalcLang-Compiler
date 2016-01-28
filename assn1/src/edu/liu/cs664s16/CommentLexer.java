9791f73730053330ce729eccfecec04b7a97cfca
Parameswara Rao Mallela <parameswararao.mallela@my.liu.edu>
2016-01-27 23:19:37 -0500

    Changed code logic to Switch case and throwing exception for incomplete comments. Test: 10 Passed.

--- assn1/src/edu/liu/cs664s16/CommentLexer.java
+++ assn1/src/edu/liu/cs664s16/CommentLexer.java
@@ -1,53 +1,43 @@
 package edu.liu.cs664s16;
 
 public class CommentLexer {
-
+    enum State { S, F }
     public static String stripComments(String source) {
-        int j = 0, k = 0;
-        String f;
-        f = "";
-        char[] y;
-        char[] z;
-        char[] x;
-        y = new char[source.length()];
-        z = new char[source.length()];
-        x = source.toCharArray();
-        boolean skip = false;
-        boolean exit1=true;
-        for (int i = 0; i < x.length; i++) {
+        State state = State.S;  // Start state
+        char [] src;
+        char [] des;
+        char [] desc;
+        int desp=0;
+        int descp=0;
+        desc = new char[source.length()];
+        des = new char[source.length()];
+        src = source.toCharArray();
+        for(int position = 0; position < src.length; position++) {
+            char current = src[position];
+            switch(state) {
+                case S:
+                    if('(' == src[position] && '*' == src[position+1]) {
+                        position++;
+                        state = State.F;}
+                    else{
+                        des[desp++] = src[position];}
+                    break;
 
-            if (x[i] == '(' && x[i + 1] == '*') {
-                skip = true;
-                z[k] = x[i];
-                k++;
-                i++;
-            } else if (x[i] == '*' && (x.length > i + 1 ? x[i + 1] == ')' : false) && skip) {
-                skip = false;
-                z = null;
-                i = i + 2;
-            }
-            if (!skip && i < x.length) {
-                y[j] = x[i];
-                j++;
-            } else if (skip && i < x.length) {
-                z[k] = x[i];
-                k++;
+                case F:
+                    if('*' == src[position] && ')' == src[position+1]) {
+                        position++;
+                        state = State.S;}
+                    else {
+                        desc[descp++] =src[position];}
+                    break;
+
+
             }
         }
-        if (skip) {
-            //Append z to Y
-            String zstr = new String(z);
-            String yystr = new String(y);
-            zstr.trim();
-            yystr.trim();
-            if (zstr != null) {
-                f = yystr + zstr;
-                exit1=false;
-            }
-        }
-        String ystr = new String(y);
-        ystr.trim();
-        if (exit1) f = ystr;
-        return f.trim();
+        if (state == State.F)
+        {throw new UnterminatedCommentExn();}
+        //System.out.println(desc);
+        String dess = new String(des);
+        return dess.trim();
     }
 }
