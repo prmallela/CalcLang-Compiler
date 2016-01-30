c4588bae1aac90b9731a0cff7dc7d850a5b9c5b1
Christopher League <league@contrapunctus.net>
2016-01-29 22:20:07 -0500

    Update assn1 tests

--- assn1/src/edu/liu/cs664s16/CommentLexerTest.java
+++ assn1/src/edu/liu/cs664s16/CommentLexerTest.java
@@ -1,10 +1,17 @@
 package edu.liu.cs664s16;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
-public class CommentLexerTest  {
+public class CommentLexerTest {
+
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(5);
 
     private void assertUnchanged(String s) {
         assertEquals(s, CommentLexer.stripComments(s));
@@ -40,6 +47,11 @@ public class CommentLexerTest  {
     }
 
     @Test
+    public void testAlmostReopen() {
+        assertChanged("This (*is comment*) :(", "This  :(");
+    }
+
+    @Test
     public void testEndNoBegin() {
         assertUnchanged("This is *) not a comment.");
     }
@@ -69,6 +81,30 @@ public class CommentLexerTest  {
     }
 
     @Test
+    public void testCommentWith2ndOpen() {
+        assertChanged(
+                "Hello (* comment (* same comment *) regular *)",
+                "Hello  regular *)"
+        );
+    }
+
+    @Test
+    public void testCommentDoubleParen() {
+        assertChanged(
+                "Hello ((*comment*))okay",
+                "Hello ()okay"
+        );
+    }
+
+    @Test
+    public void testCommentDoubleStar() {
+        assertChanged(
+                "Hello (*comment**)okay",
+                "Hello okay"
+        );
+    }
+
+    @Test
     public void testCommentWith2ndClose() {
         assertChanged(
                 "Hello (* comment *) text *) again",
@@ -80,4 +116,27 @@ public class CommentLexerTest  {
     public void testUnterminatedComment() {
         CommentLexer.stripComments("Foo (* unending");
     }
+
+    @Test(expected = UnterminatedCommentExn.class)
+    public void testAlmostClose() {
+        CommentLexer.stripComments("Foo (* unending*");
+    }
+
+    @Test
+    public void testReallyLongString() {
+        Random rng = new Random();
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < 4096; i++) {
+            buf.append('a' + rng.nextInt(26)); // Random letters
+        }
+        String expected = buf.toString();
+        String code = "(* random! *)" + expected;
+        assertChanged(code, expected);
+    }
+
+    @Test
+    public void testMultipleComments() {
+        assertChanged("Be(*1*)By(*2*)Bo", "BeByBo");
+    }
+
 }
