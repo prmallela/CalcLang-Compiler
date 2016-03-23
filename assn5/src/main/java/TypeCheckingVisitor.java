8e65a3f362fd0163e541af9d314dcbd4a56520c7
Parameswara Rao Mallela <mallelapr@gmail.com>
2016-03-23 14:58:27 -0400

    assn5

--- assn5/src/main/java/TypeCheckingVisitor.java
+++ assn5/src/main/java/TypeCheckingVisitor.java
@@ -1,9 +1,12 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.misc.Interval;
 import org.antlr.v4.runtime.misc.Nullable;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.gui.SystemFontMetrics;
 
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class TypeCheckingVisitor extends CalcLangBaseVisitor<Type> {
@@ -41,26 +44,96 @@ public class TypeCheckingVisitor extends CalcLangBaseVisitor<Type> {
     }
 
     @Override
+    public Type visitIfExpr(CalcLangParser.IfExprContext ctx){
+        try {
+            String b = ctx.expr(0).getText();
+            String th = ctx.expr(1).getText();
+            //int th1 = Integer.parseInt(th);
+            String el=ctx.expr(2).getText();
+            //int el1 = Integer.parseInt(el);
+            if(b.equals("true")||b.equals("false")){
+                if((th.equals("true") && el.equals("false"))
+                        ||(el.equals("true") && th.equals("false"))  ){
+                    return trace(ctx,Type.BOOL);
+                }
+                else if (isNumeric(th) && isNumeric(el)){
+                    return trace(ctx,Type.BOOL);
+                }
+                else
+                    return typeMismatch(ctx,Type.BOOL, Type.INT);
+
+            }
+            else
+                return typeMismatch(ctx,Type.BOOL, Type.INT);
+
+        }
+        catch (Exception x){
+            return typeMismatch(ctx,Type.BOOL, Type.INT);
+        }
+    }
+
+    public static boolean isNumeric(String str)
+    {
+        try
+        {
+            double d = Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
+    }
+    @Override
     public Type visitOpExpr(CalcLangParser.OpExprContext ctx) {
         Type leftType = ctx.left.accept(this);
         Type rightType = ctx.right.accept(this);
-        switch (ctx.op.getText()) {
-            case "^":
-            case "*":
-            case "/":
-            case "+":
-            case "-":
-                if ((leftType == Type.INT || leftType == Type.FLOAT) &&
-                        (rightType == Type.INT || rightType == Type.FLOAT)) {
-                    return trace(ctx, leastUpperBound(leftType, rightType));
-                } else {
+        if (ctx.op.getText() != null && leftType == Type.BOOL && rightType == Type.BOOL) {
+            switch (ctx.op.getText()) {
+                case "-":case "^":case "*":case "+":
                     return typeMismatch(ctx, leftType, rightType);
-                }
-
                 default:
-                    return trace(ctx,leftType);
+                    return trace(ctx, leftType);
+            }
         }
-    }
+        else if ((leftType ==Type.BOOL && rightType==Type.INT)
+                || (leftType==Type.BOOL && rightType==Type.FLOAT)
+                || (leftType==Type.INT && rightType==Type.BOOL)
+                || (leftType==Type.FLOAT && rightType==Type.BOOL)){
+                switch (ctx.op.getText()){
+                    case "/":case "^":case "+":case"<":case">=":case">":case"<=":case"=":case"<>":
+                    case "&":
+
+                        return typeMismatch(ctx,leftType,rightType);
+                    default:
+                        return trace(ctx,leftType);
+                }
+        }
+        else if ((leftType == Type.INT || leftType == Type.FLOAT) &&
+                (rightType == Type.INT || rightType == Type.FLOAT)) {
+            switch (ctx.op.getText()) {
+                case "^":
+                case "*":
+                case "/":
+                case "+":
+                case "-":
+               // case "&":
+                    if ((leftType == Type.INT || leftType == Type.FLOAT) &&
+                            (rightType == Type.INT || rightType == Type.FLOAT))
+                        return trace(ctx, leastUpperBound(leftType, rightType));
+
+                    else {
+                        return typeMismatch(ctx, leftType, rightType);
+                    }
+                case "|":case "&":
+                    return typeMismatch(ctx,leftType,rightType);
+                default:
+                    return trace(ctx, leftType);
+            }
+        }
+        return trace(ctx,leftType);
+        }
+
 
     public static Type leastUpperBound(Type t1, Type t2)
     {
@@ -112,10 +185,22 @@ public class TypeCheckingVisitor extends CalcLangBaseVisitor<Type> {
             case "floor":  // FLOAT → INT
                 if(checkNumArgs(ctx, name, 1, ctx.expr().size())) {
                     Type type = ctx.expr(0).accept(this);
-                    if(type == Type.FLOAT) return trace(ctx, Type.INT);
+                    if(type == Type.FLOAT ||type==Type.INT) return trace(ctx,type);
                     else return typeMismatch(ctx, type, Type.FLOAT);
                 }
-            default:
+            case "log":
+                if(checkNumArgs(ctx, name, 2, ctx.expr().size())) {
+                    Type type = ctx.expr(0).accept(this);
+                    if(type==Type.INT ||type==Type.FLOAT) return trace(ctx,type);
+                    else return typeMismatch(ctx,type,Type.FLOAT);
+                }
+            case "sqrt":
+                if(checkNumArgs(ctx, name, 1, ctx.expr().size())) {
+                    Type type = ctx.expr(0).accept(this);
+                    if(type==Type.INT ||type==Type.FLOAT) return trace(ctx,type);
+                    else return typeMismatch(ctx,type,Type.FLOAT);
+                }
+                    default:
                 error(ctx, "undefined function: " + name);
         }
         return trace(ctx, Type.ERROR);
